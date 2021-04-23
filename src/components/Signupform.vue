8a8585dc4c71fd7f4da133fd31a380c466e7987e
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-04-23 01:54:10 -0300

    added select box

--- src/components/Signupform.vue
+++ src/components/Signupform.vue
@@ -5,10 +5,17 @@
 
 		<label for="password">Password</label>
 		<input type="password" name="password" required v-model="password"/>
+
+		<label for="password">Role</label>
+		<select name="role">
+			<option value="developer">Web Developer</option>
+			<option value="designer">Web Designer</option>
+		</select>
 	</form>
 
 	<p>E-mail: {{ email }}</p>
 	<p>Password: {{ password }}</p>
+	<p>Role: {{ role }}</p>
 </template>
 
 <script>
@@ -16,7 +23,8 @@ export default {
 	data() {
 		return {
 			email: '',
-			password: ''
+			password: '',
+			role: ''
 		}
 	}
 }
@@ -40,7 +48,7 @@ export default {
 		letter-spacing: 1px;
 		font-weight: bold;
 	}
-	input {
+	input, select {
 		display: block;
 		padding: 10px 6px;
 		width: 100%;
